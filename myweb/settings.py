0d17a1f8de23d1f77696b5202e15416d8d7f56e4
5hseok <ohhs1010@gmail.com>
2023-12-26 16:50:50 +0900

    example app 등록 및 migrate

--- myweb/settings.py
+++ myweb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photo'
+    'rest_framework',
+    'example',
 ]
 
 MIDDLEWARE = [
