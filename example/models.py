07fc0cf88a9bd808bce38bcea1a950d5b1490ecf
5hseok <ohhs1010@gmail.com>
2023-12-29 16:55:23 +0900

    Book model migration

--- example/models.py
+++ example/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    bid = models.IntegerField(primary_key=True) # 책 id
+    title = models.CharField(max_length=50) # book title
+    author = models.CharField(max_length=50) # author
+    category = models.CharField(max_length=50) # category
+    pages = models.IntegerField() # pages
+    price = models.IntegerField() # price
+    published_date = models.DateField() # published_date
+    description = models.TextField() # description
