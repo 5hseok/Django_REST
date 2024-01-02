9618b7a75669654c384fe45994489b66e8f8463d
5hseok <ohhs1010@gmail.com>
2024-01-02 22:57:29 +0900

    도서 정보 API 등록

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import HelloAPI
+from .views import HelloAPI, bookAPI, booksAPI, BookAPI, BooksAPI
 
 urlpatterns = [
-    path("hello/", HelloAPI)
+    path("hello/", HelloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/books/<int:bid>/",bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),             # CBV의 뷰를 path에 등록할 때는 .as_view를 사용한다.
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
