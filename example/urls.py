dfcf3d07a94ef1a39166111957b84966394670ce
5hseok <ohhs1010@gmail.com>
2024-01-02 22:58:50 +0900

    Fix : Url 변경 fbv/books/1을 fbv/book/1로 변경

--- example/urls.py
+++ example/urls.py
@@ -4,7 +4,7 @@ from .views import HelloAPI, bookAPI, booksAPI, BookAPI, BooksAPI
 urlpatterns = [
     path("hello/", HelloAPI),
     path("fbv/books/", booksAPI),
-    path("fbv/books/<int:bid>/",bookAPI),
+    path("fbv/book/<int:bid>/",bookAPI),
     path("cbv/books/", BooksAPI.as_view()),             # CBV의 뷰를 path에 등록할 때는 .as_view를 사용한다.
     path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
