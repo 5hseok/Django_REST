e5721bf92f8e57caad8b400ba71f453e0c7e0c89
5hseok <ohhs1010@gmail.com>
2024-01-02 22:57:17 +0900

    Fix : Serializer field 수정 publish_date를 published_date로 수정

--- example/serializers.py
+++ example/serializers.py
@@ -4,5 +4,5 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['bid', 'title', 'author', 'category', 'pages', 'price', 'publish_date', 'description']
+        fields = ['bid', 'title', 'author', 'category', 'pages', 'price', 'published_date', 'description']
     
