10ffe02f9ca2a01544a014b35a771e8fc061cfc7
5hseok <ohhs1010@gmail.com>
2024-01-03 15:10:25 +0900

    mixin을 사용하여 api 정의

--- example/views.py
+++ example/views.py
@@ -47,3 +47,26 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+# mixins 사용
+from rest_framework import generics
+from rest_framework import mixins
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+
+	def get(self, request, *args, **kwargs): # GET 메소드 처리 함수(전체 목록)
+		return self.list(request, *args, **kwargs)  # mixins.ListModelMixin과 연결
+	def post(self, request, *args, **kwargs): # POST 메소드 처리 함수(1권 등록)
+		return self.create(request, *args, **kwargs) #mixins.CreateModelMixin과 연결
+class BookAPIMixins(mixins.RetrieveModelMixins, mixins.UpdateModelMixin, mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+    #django 기본 모델 pk가 아닌 bid를 pk로 사용하고 있으므로 lookup_field로 설정
+    def get(self, request, *args, **kwargs):  # GET 메소드 처리 함수(1권)
+        return self.retrieve(request, *args, **kwargs) #mixins.RetrieveModelMixin과 연결
+    def put(self, request, *args, **kwarges): # PUT 메소드 처리 함수(1권 수정)
+        return self.update(request, *args, **kwarges) # mixins.UpdateModelMixin과 연결
+    def delete(self, request, *args, **kwargs): # DELETE 메소드 처리 함수(1권 삭제)
+        return self.destroy(request, *args, **kwargs) # mixins.DestroyModelMixin과 연결
